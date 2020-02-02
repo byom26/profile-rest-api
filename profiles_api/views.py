dffd40de18fb60d3c42fb27a3acbf44825ebbe7d
byom26 <p.byom26@gmail.com>
2020-02-02 20:56:01 +0530

    Added viewset to profiles_api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from . import serializers
 
 
@@ -41,3 +41,48 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         '''Delete an object'''
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    '''Test API Viewset'''
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        '''Returns a hello message'''
+        a_viewset = [
+            'Uses actions (list, create, retrive, update, partial_update)',
+            'Automatically maps to urls using routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'messahe': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        '''Create a new hello message'''
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrive(self, request, pk=None):
+        '''Handle getting an object by its ID'''
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        '''Handle updating an object by its ID'''
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        '''Handle updating part of an object by its ID'''
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        '''Handle removing an object by its ID'''
+        return Response({'http_method': 'DELETE'})
