05fce7d3ff9df4f996e2ddf4b76fccda9b5e7f42
byom26 <p.byom26@gmail.com>
2020-02-05 11:59:03 +0530

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,10 +5,11 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet) 
+router.register('profile', views.UserProfileViewSet)
 #Here we are not using a base_name since we are using a queryset & django automatically figures out a base name. If we want to over write the default base name, then we specify one.
 
 urlpatterns = [
-    path('hello-view', views.HelloApiView.as_view()),
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('login', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
